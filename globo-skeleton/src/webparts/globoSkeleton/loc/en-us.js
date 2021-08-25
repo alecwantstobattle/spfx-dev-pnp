20b6694e8a2b69e99bc2fe9d0f5459e761ab71e2
Alec Tecson <tecsonalr@gmail.com>
2021-08-25 22:54:05 +0800

    Setup to Retrieve the User's Agenda Using PnP

--- globo-skeleton/src/webparts/globoSkeleton/loc/en-us.js
+++ globo-skeleton/src/webparts/globoSkeleton/loc/en-us.js
@@ -2,6 +2,15 @@ define([], function() {
   return {
     "PropertyPaneDescription": "Description",
     "BasicGroupName": "Group Name",
-    "DescriptionFieldLabel": "Description Field"
+    "DescriptionFieldLabel": "Description Field",
+    "AllDay": "All day",
+    "Error": "An error has occured while retrieving your upcoming meetings",
+    "Heading": "Today's Meetings",
+    "Hour": "hour",
+    "Hours": "hours",
+    "Loading": "Retrieving your upcoming meetings",
+    "Minutes": "minutes",
+    "NoMessages": "You have no upcoming meetings",
+    "ViewAll": "View the full list"
   }
 });
--- globo-skeleton/src/webparts/globoSkeleton/loc/mystrings.d.ts
+++ globo-skeleton/src/webparts/globoSkeleton/loc/mystrings.d.ts
@@ -2,6 +2,15 @@ declare interface IGloboSkeletonWebPartStrings {
   PropertyPaneDescription: string;
   BasicGroupName: string;
   DescriptionFieldLabel: string;
+  AllDay: string;
+  Error: string;
+  Heading: string;
+  Hour: string;
+  Hours: string;
+  Loading: string;
+  Minutes: string;
+  NoMeetings: string;
+  ViewAll: string;
 }
 
 declare module 'GloboSkeletonWebPartStrings' {
